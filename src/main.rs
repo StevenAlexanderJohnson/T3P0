41e17eba246083f5b9506c7c0efd7df65ab9fcf0
Steven Johnson <steveajohn1@gmail.com>
2024-07-31 20:17:21 +0000

    starting tcp server in main

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,89 @@
-use t3p0::request::{DataRequest, Request};
+use std::{collections::HashMap, sync::Arc};
+use t3p0::{game_state::PlayerNumber, request::Request, DataRequest, GameState, GameStateTrait};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+    sync::{mpsc, Mutex},
+};
 
-fn main() {
-    let bitmask: u32 = u32::MAX;
-    println!("Bitmask: {:#034b}", bitmask);
+#[derive(Debug)]
+enum GameRequest {
+    GetState {
+        game_id: u32,
+        response: mpsc::Sender<GameState>,
+    },
+    UpdateState {
+        game_id: u32,
+        new_state: GameState,
+    },
+}
 
-    let r = Request(u32::MAX);
-    let turn = r.get_turn();
-    println!("Turn: {:#034b}, {}", turn, turn);
-    println!("Is P2 Turn: {}", r.get_is_p2_turn());
-    println!("Swapping Players: {:#034b}", r.swap_player().0);
-    let r = r.swap_player();
-    println!("Is p2 Turn: {}", r.get_is_p2_turn());
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8000").await?;
+    let (tx, mut rx) = mpsc::channel(32);
+    let game_state_map = Arc::new(Mutex::new(HashMap::<u32, GameState>::new()));
 
-    println!("{}", r.get_message_number());
-
-    let board_state = r.get_board_state();
-    println!("{:#034b}, {}", board_state, board_state);
-
-    let mut r = Request::new_data_request(false);
-    println!(
-        "0: Message -> {}, Turn -> {}, Player 2 -> {}, {:#034b}",
-        r.get_message_number(),
-        r.get_turn(),
-        r.get_is_p2_turn(),
-        r.0
-    );
-    for i in 0..29 {
-        r = match r.increment_turn_and_message() {
-            Ok(r) => r,
-            Err(e) => {
-                println!("{}", e);
-                break;
+    let game_state_map_clone = game_state_map.clone();
+    tokio::spawn(async move {
+        while let Some(request) = rx.recv().await {
+            let mut state = game_state_map_clone.lock().await;
+            match request {
+                GameRequest::GetState { game_id, response } => {
+                    let game_state = state.get(&game_id).cloned();
+                    if let Some(game_state) = game_state {
+                        let _ = response.send(game_state);
+                    }
+                }
+                GameRequest::UpdateState { game_id, new_state } => {
+                    state.insert(game_id, new_state);
+                }
             }
-        };
+        }
+    });
 
-        println!(
-            "{}: Message -> {}, Turn -> {}, Player 2 -> {}, {:#034b}",
-            i + 1,
-            r.get_message_number(),
-            r.get_turn(),
-            r.get_is_p2_turn(),
-            r.0
-        );
+    loop {
+        let (socket, _) = listener.accept().await?;
+        let tx_clone = tx.clone();
+        tokio::spawn(async move {
+            if let Err(e) = handle_connection(socket, tx_clone).await {
+                eprintln!("Error: {:?}", e);
+            }
+        });
     }
+}
+
+async fn handle_connection(
+    mut socket: TcpStream,
+    tx: mpsc::Sender<GameRequest>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut buffer = [0u8; 4];
+
+    loop {
+        let n = socket.read(&mut buffer).await?;
+        if n == 0 {
+            break;
+        }
+
+        let request: Request = Request(u32::from_be_bytes(buffer));
+        let (response_tx, mut response_rx) = mpsc::channel::<GameState>(1);
+        tx.send(GameRequest::GetState {
+            game_id: request.0,
+            response: response_tx,
+        })
+        .await?;
+
+        if let Some(game_state) = response_rx.recv().await {
+            tx.send(GameRequest::UpdateState {
+                game_id: 1u32,
+                new_state: GameState::from_request(request, PlayerNumber(2))?,
+            })
+            .await?;
+
+            socket
+                .write(&Request::new_data_request(true).0.to_be_bytes())
+                .await?;
+        }
+    }
+    Ok(())
 }
