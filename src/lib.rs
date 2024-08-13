a975672012eb7f9e314bbb628ada07e07cdfda7b
Steven Johnson <steveajohn1@gmail.com>
2024-08-13 18:33:52 +0000

    adding new struct to represent the connection

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@ pub mod game_state;
 pub mod player;
 pub mod request;
 pub mod game_server;
+pub mod game_connection;
 
 pub use game_state::{GameState, GameStateTrait};
 pub use player::{Player, PlayerTrait};
 pub use request::DataRequest;
 pub use game_server::{GameServer, GameServerTrait, GameRequest};
+pub use game_connection::GameConnection;
