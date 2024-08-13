11b1d57d9be6c702f3b92d1135e540b8e825f4ab
Steven Johnson <steveajohn1@gmail.com>
2024-08-13 20:03:46 +0000

    running cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
+pub mod game_connection;
+pub mod game_server;
 pub mod game_state;
 pub mod player;
 pub mod request;
-pub mod game_server;
-pub mod game_connection;
 
+pub use game_connection::GameConnection;
+pub use game_server::{GameRequest, GameServer, GameServerTrait};
 pub use game_state::{GameState, GameStateTrait};
 pub use player::{Player, PlayerTrait};
 pub use request::DataRequest;
-pub use game_server::{GameServer, GameServerTrait, GameRequest};
-pub use game_connection::GameConnection;
