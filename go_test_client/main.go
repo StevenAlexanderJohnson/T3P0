e8f6911ea89be870bc58780027ce27d6cca49606
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2024-08-13 22:18:17 -0400

    adding go test client

---
+++ go_test_client/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+
+	"github.com/google/uuid"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+	defer conn.Close()
+
+	uuid, err := performHandshake(conn, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%v\n", uuid)
+
+	var line string
+	fmt.Scanf(line)
+}
+
+func checkOkSignal(buffer []byte) bool {
+	return binary.BigEndian.Uint32(buffer[:4]) == 1<<31
+}
+
+func performHandshake(connection net.Conn, preferredId *uuid.UUID) (*uuid.UUID, error) {
+	playerId := uuid.New()
+	if preferredId != nil {
+		playerId = *preferredId
+	}
+	buffer := make([]byte, 16)
+
+	// Send initial Hello message
+	binary.BigEndian.PutUint32(buffer[:4], uint32(1<<31))
+	connection.Write(buffer[:4])
+
+	// If we have a preferred Id we should discard the next message and send our preferred Id
+	// Else just send another ok message to say we received our Id
+	if preferredId != nil {
+		buffer, err := preferredId.MarshalBinary()
+		if err != nil {
+			panic("Unable to parse preferredId")
+		}
+		connection.Write(buffer)
+
+		// Check that the response from the server is ok
+		n, err := connection.Read(buffer)
+		if err != nil || n != 4 {
+			return nil, fmt.Errorf("server did not send a valid response to requesting player id")
+		}
+
+		if !checkOkSignal(buffer) {
+			return nil, fmt.Errorf("server did not respond with ok request to player id")
+		}
+	} else {
+		n, err := connection.Read(buffer)
+		if err != nil || n != 16 {
+			return nil, fmt.Errorf("server did not send a valid request to ok message")
+		}
+
+		receivedUUID, err := uuid.FromBytes(buffer)
+		if err != nil {
+			return nil, fmt.Errorf("server did not send a valid player id")
+		}
+		playerId = receivedUUID
+		binary.BigEndian.PutUint32(buffer[:4], uint32(1<<31))
+		connection.Write(buffer[:4])
+	}
+
+	return &playerId, nil
+}
