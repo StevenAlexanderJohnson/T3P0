53035c955cb5afb159887215346f4b3b087a37ea
Steven Johnson <steveajohn1@gmail.com>
2024-08-21 18:02:04 +0000

    writing test in player.rs

--- src/player.rs
+++ src/player.rs
@@ -22,3 +22,34 @@ impl PlayerTrait for Player {
         Player(*Uuid::from_bytes_ref(bytes))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let player = Player::new();
+        assert_eq!(player.get_id().to_string().len(), 36);
+    }
+
+    #[test]
+    fn test_from_bytes() {
+        let bytes = [
+            0x0b, 0x0e, 0x0e, 0x0f, 0x0b, 0x0e, 0x0e, 0x0f, 0x0b, 0x0e, 0x0e, 0x0f, 0x0b, 0x0e,
+            0x0e, 0x0f,
+        ];
+        let player = Player::from_bytes(&bytes);
+        assert_eq!(player.0.to_string(), "0b0e0e0f-0b0e-0e0f-0b0e-0e0f0b0e0e0f");
+    }
+
+    #[test]
+    fn test_get_id() {
+        let bytes = [
+            0x0b, 0x0e, 0x0e, 0x0f, 0x0b, 0x0e, 0x0e, 0x0f, 0x0b, 0x0e, 0x0e, 0x0f, 0x0b, 0x0e,
+            0x0e, 0x0f,
+        ];
+        let player = Player::from_bytes(&bytes);
+        assert_eq!(player.get_id().to_string(), "0b0e0e0f-0b0e-0e0f-0b0e-0e0f0b0e0e0f");
+    }
+}
